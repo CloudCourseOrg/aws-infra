5149fd102e3b227520f61e84f80a0cc7efac0275
Arpit Jain <jain.arpit@northeastern.edu>
2023-04-13 02:43:49 -0400

    Final review

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -35,11 +35,19 @@ resource "aws_lb_target_group" "alb_target_group" {
 # create a listener on port 80 with redirect action
 resource "aws_lb_listener" "alb_http_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = data.aws_acm_certificate.app_certificate.arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
+
+
+data "aws_acm_certificate" "app_certificate" {
+  domain   = var.record_creation_name
+  statuses = ["ISSUED"]
+}
+
